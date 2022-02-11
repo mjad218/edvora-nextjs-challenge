1b3bca2e467ea9a4f737df19b665723934a7dfa2
Mohamed Gad <mohamedgad149@gmail.com>
2022-02-11 20:21:25 +0200

    installed swiper for the slider

--- comps/ListProducts.js
+++ comps/ListProducts.js
@@ -1,13 +1,28 @@
 import Product from "./Product";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper";
 
-const ListProducts = ({products, category}) => {
+const ListProducts = ({ products, category }) => {
     return (
-        <div className  ="listing">
+        <div className="listing">
             <h2>
                 {category}
             </h2>
-            <div className="products">                
-                {products.map(product => <Product key={product.date} product={product}/>) }
+            <div className="products">
+                <Swiper slidesPerView={3}
+                    spaceBetween={30}
+                    freeMode={true}
+                    navigation
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[FreeMode, Pagination]}
+                    className="mySwiper">
+                    {products.map(product => <SwiperSlide key={product.date} ><Product product={product} /></SwiperSlide>)}
+                </Swiper>
             </div>
         </div>
     );
